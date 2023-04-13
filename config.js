b7924192533597c67e3a5b7a08a0d93f908be841
fabi0_pt@hotmail.com <fabi0_pt@hotmail.com>
2023-04-13 18:52:41 -0300

    asfdasfa

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const API = "https://api-production-021b.up.railway.app",
+export const API = "http://localhost:5684",
   versao = "v1",
   loja = "640d3c22c2cc3b8e1bbf2984",
   baseImg = API + "/public/img/";
